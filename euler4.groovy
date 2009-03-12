ba45bd7bfb40ae826ababb9c1a4cbff9959160b7
MBO <boruta.miroslaw+git@gmail.com>
2009-03-12 01:28:35 +0100

    euler4

--- euler4.groovy
+++ euler4.groovy
@@ -7,10 +7,27 @@
 // Find the largest palindrome made from the product of two 3-digit numbers.
 //****************************************************************************
 
-println (999..100).each { a ->
-    (a..100).each { b ->
-        def str = (a*b).toString()
-        if (str == str.split('').reverse().join('')) return str
+//   9  8  7  6  5  4  3  2  1
+// 9 81 72 63 54 45 36 27 18  9
+// 8    64 56 48 40 32 24 16  8
+// 7       49 42 35 28 21 14  7
+// 6          36 30 24 18 12  6
+// 5             25 20 15 10  5
+// 4                16 12  8  4
+// 3                    9  6  3
+// 2                       4  2
+// 1                          1
+//
+// 99 98 88 97 87 96 77 88 ...
+
+def nums = []
+(100..999).each { a ->
+    (100..a).each { b ->
+        nums << a*b
     }
 }
+def res = nums.sort().reverse().find {
+    it.toString() == it.toString().split('').reverse().join('')
+}
+println res
 
