9aca7f624804d29783e2d1861cbc772568a19c54
MBO <boruta.miroslaw+git@gmail.com>
2009-04-25 18:15:59 +0200

    this was sick, i should try it the imperative way

---
+++ euler5.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+#***************************************************************************
+# 2520 is the smallest number that can be divided by each of the numbers
+# from 1 to 10 without any remainder.
+#
+# What is the smallest number that is evenly divisible by all of the numbers
+# from 1 to 20?
+#***************************************************************************
+
+def primes(num)
+  sqrt = Math.sqrt(num).floor
+  array = Array.new(num,0)
+  (2..sqrt).each do |i|
+    while num % i == 0
+      num /= i
+      array[i-1] += 1
+    end
+  end
+  array[1-1] = 1
+  array[num-1] = 1
+  array
+end
+
+def euler5(max)
+  (2..max).inject(Array.new(max,0)) do |acc,el|
+    acc.zip(primes(el)).collect { |xs| xs[1] ? xs.max : xs[0] }
+  end.zip((1..20).to_a).collect { |xs| xs[1] ** xs[0] }.reduce(:*)
+end
+
+puts euler5(20).inspect
+
